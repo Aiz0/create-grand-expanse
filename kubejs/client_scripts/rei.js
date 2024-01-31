f182965c096d4233aa88d1172aaa2b1e51fd8d81
Aiz <66974576+Aiz0@users.noreply.github.com>
2024-01-31 23:24:23 +0100

    feat(kubejs): REI groups (#28)
    * feat(kubejs): REI groups
    
    Makes some REI Groups to reduce clutter.
    
    * Prettified Code!
    
    ---------
    
    Co-authored-by: Aiz0 <Aiz0@users.noreply.github.com>

---
+++ kubejs/client_scripts/rei.js
@@ -0,0 +1,45 @@
+REIEvents.groupEntries((event) => {
+    // Rechiseled and RechiseledCreate
+    event.groupItems(
+        "kubejs:rei_groups/rechiseled",
+        "Rechiseled",
+        Ingredient.of(/^rechiseled(create)?:/).itemIds.filter(
+            (id) =>
+                ![
+                    "rechiseled:chisel",
+                    "rechiseledcreate:mechanical_chisel",
+                ].includes(id),
+        ),
+    );
+
+    // Create Deco Bricks
+    event.groupItems(
+        "kubejs:rei_groups/createdeco_bricks",
+        "Create Deco Bricks",
+        Ingredient.of(/^createdeco:/).itemIds.filter((id) =>
+            id.includes("brick"),
+        ),
+    );
+
+    const useNbt = [
+        "potion",
+        "enchanted_book",
+        "splash_potion",
+        "tipped_arrow",
+        "lingering_potion",
+    ];
+
+    useNbt.forEach((id) => {
+        const item = Item.of(id);
+        const { namespace, path } = Utils.id(item.id);
+        event.groupSameItem(
+            `kubejs:rei_groups/${namespace}/${path}`,
+            item.name,
+            item,
+        );
+    });
+
+    event.groupItems("kubejs:rei_groups/spawn_eggs", "Spawn Eggs", [
+        /spawn_egg/,
+    ]);
+});
