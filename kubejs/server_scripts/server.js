7372eaa1df837d6df2d5092755d8f33ead65ec5c
Aiz <dev@aiz.moe>
2024-02-02 22:51:55 +0100

    feat(recipes): remove tools
    removes iron and gold tool crafting.  fixes backpack recipe as secret

--- kubejs/server_scripts/server.js
+++ kubejs/server_scripts/server.js
@@ -1,13 +1,29 @@
 ServerEvents.recipes((event) => {
-    event.shaped(
-        Item.of("sophisticatedbackpacks:backpack", 5),
-        ["ABA", "CDC", "EBE"],
-        {
-            A: "minecraft:leather",
-            B: "farmersdelight:canvas",
-            C: "farmersdelight:rope",
-            D: "minecraft:barrel",
-            E: "minecraft:bundle",
-        }
-    ).id("sophisticatedbackpacks:backpack");
+    event
+        .shaped(
+            Item.of("sophisticatedbackpacks:backpack", 1),
+            ["ABA", "CDC", "EBE"],
+            {
+                A: "minecraft:leather",
+                B: "farmersdelight:canvas",
+                C: "farmersdelight:rope",
+                D: "minecraft:barrel",
+                E: "minecraft:bundle",
+            }
+        )
+        .id("sophisticatedbackpacks:backpack");
+
+    // Remove crafting of iron and gold tools
+    // We want alternate recipes for iron tools later
+    // gold should only be found as loot
+    event.remove([
+        { output: "#forge:tools/iron" },
+        { output: "#forge:tools/gold" },
+    ]);
+
+    // Remove ad astra hammer.
+    event.remove([
+        { id: "ad_astra:recipes/hammer" },
+        { input: "ad_astra:hammer" },
+    ]);
 });
