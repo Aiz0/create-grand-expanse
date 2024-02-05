4f9b9f3d8b031d69577997143a9b9775ac0c3ed6
Aiz <dev@aiz.moe>
2024-02-05 13:30:25 +0100

    feat(fluid-tags): add lava to our molten fluids

--- kubejs/server_scripts/fluid-tags.js
+++ kubejs/server_scripts/fluid-tags.js
@@ -1,4 +1,15 @@
 ServerEvents.tags("fluid", (event) => {
-    event.add("minecraft:lava", global.fluids.molten_tin);
-    event.add("minecraft:lava", "grandexpanse:flowing_molten_tin");
+    // For now just add lava tag to all molten fluids
+
+    for (const [name, id] of Object.entries(global.fluids)) {
+        if (!name.startsWith("molten")) {
+            continue;
+        }
+        // insert ":flowing_" into the id
+        let flowingFluid = id.split(":");
+        flowingFluid.splice(1, 0, ":flowing_");
+        flowingFluid = flowingFluid.join("");
+        event.add("minecraft:lava", id);
+        event.add("minecraft:lava", flowingFluid);
+    }
 });
