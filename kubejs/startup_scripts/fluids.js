26669cb039a9978ec792eddd434b41b90e8fa1f4
Aiz <dev@aiz.moe>
2024-02-05 13:31:04 +0100

    feat(fluids): create all our fluids

--- kubejs/startup_scripts/fluids.js
+++ kubejs/startup_scripts/fluids.js
@@ -1,9 +1,16 @@
 StartupEvents.registry("fluid", (event) => {
+    function molten_metal(fluid, color) {
+        event
+            .create(fluid)
+            .stillTexture("kubejs:block/fluid/molten/still")
+            .flowingTexture("kubejs:block/fluid/molten/flowing")
+            .color(color);
+    }
+
     //create fluids here
-    event
-        .create(global.fluids.molten_tin)
-        .thickTexture(0xb6d5e6)
-        .bucketColor(0xb6d5e6)
-        .stillTexture("kubejs:block/fluid/molten/still")
-        .flowingTexture("kubejs:block/fluid/molten/flowing");
+    molten_metal(global.fluids.molten_tin, 0x7ea0b5);
+    molten_metal(global.fluids.molten_nickel, 0xbdac78);
+    molten_metal(global.fluids.molten_desh, 0xd68d4d);
+    molten_metal(global.fluids.molten_silver, 0xb4c0db);
+    molten_metal(global.fluids.molten_lead, 0x312d43);
 });
