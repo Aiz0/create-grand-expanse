020fbde57d63b4584ab3f782b6a8b4840aa892cc
Aiz <66974576+Aiz0@users.noreply.github.com>
2024-02-06 12:26:20 +0100

    wadd custom fluids (#45)d
    * feat(textures): molten textures
    
    copied from tinker's construct.
    
    * feat: molten tin
    
    adds molten tin and melting tin recipes
    
    * feat(tags): lava tag to tin
    
    * feat(fluid-tags): add lava to our molten fluids
    
    * feat(fluids): create all our fluids
    
    * fix(fluids): remove unneeded fluid
    
    molten steel is defined by createmetallurgy.

---
+++ kubejs/server_scripts/fluid-tags.js
@@ -0,0 +1,15 @@
+ServerEvents.tags("fluid", (event) => {
+    // For now just add lava tag to all molten fluids
+
+    for (const [name, id] of Object.entries(global.fluids)) {
+        if (!name.startsWith("molten")) {
+            continue;
+        }
+        // insert ":flowing_" into the id
+        let flowingFluid = id.split(":");
+        flowingFluid.splice(1, 0, ":flowing_");
+        flowingFluid = flowingFluid.join("");
+        event.add("minecraft:lava", id);
+        event.add("minecraft:lava", flowingFluid);
+    }
+});
---
+++ kubejs/startup_scripts/fluids.js
@@ -0,0 +1,16 @@
+StartupEvents.registry("fluid", (event) => {
+    function molten_metal(fluid, color) {
+        event
+            .create(fluid)
+            .stillTexture("kubejs:block/fluid/molten/still")
+            .flowingTexture("kubejs:block/fluid/molten/flowing")
+            .color(color);
+    }
+
+    //create fluids here
+    molten_metal(global.fluids.molten_tin, 0x7ea0b5);
+    molten_metal(global.fluids.molten_nickel, 0xbdac78);
+    molten_metal(global.fluids.molten_desh, 0xd68d4d);
+    molten_metal(global.fluids.molten_silver, 0xb4c0db);
+    molten_metal(global.fluids.molten_lead, 0x312d43);
+});
