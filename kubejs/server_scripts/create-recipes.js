577ed581a871736b641da223bb625397981013b6
Aiz <66974576+Aiz0@users.noreply.github.com>
2024-02-10 21:09:49 +0100

    Refactor plus dusts tagging actually (#86)
    * refactor(melting): better structure to it.
    
    * refactor(kubejs): more files
    
    * refactor(kubejs): defined items
    
    defining items is now done in the global object directly instead of an array. This makes it easier to type and for code editors to autocomplete them. no more mistakes.

---
+++ kubejs/server_scripts/create-recipes.js
@@ -0,0 +1,19 @@
+ServerEvents.recipes((event) => {
+    // All recipes using create should be done in here.
+
+    // Mixing Recipes.
+    // Will be replaced with helper function later.
+    event.recipes.createMixing(
+        Fluid.of(global.fluids.molten_rose_gold, FluidAmounts.NUGGET),
+        [
+            {
+                fluid: "createmetallurgy:molten_gold",
+                amount: FluidAmounts.NUGGET,
+            },
+            {
+                fluid: "createmetallurgy:molten_copper",
+                amount: FluidAmounts.NUGGET,
+            },
+        ]
+    );
+});
---
+++ kubejs/server_scripts/metallurgy.js
@@ -0,0 +1,64 @@
+ServerEvents.recipes((event) => {
+    // Adds smelting recipes to the smeltable ores
+    global.config.furnace.ores.forEach(smelt_to_nuggets);
+
+    // Adds melting to the meltable materials
+    global.config.melting.materials.forEach((material) => {
+        global.config.melting.items.forEach((item) => {
+            const tag = item.tag.concat(material.name);
+            if (Item.of(`#${tag}`).empty) {
+                return;
+            }
+            melting(
+                tag,
+                material.fluid,
+                item.fluidAmount,
+                material.heatRequirement
+            );
+        });
+    });
+
+    // Helper Functions
+    function smelt_to_nuggets(material) {
+        // Normal Smelting
+        event.smelting(
+            `${global.config.furnace.smelting_nuggets}x #forge:nuggets/${material}`,
+            `#forge:raw_materials/${material}`
+        );
+        // Blasting. A bit Faster, A bit Better.
+        event.blasting(
+            `${global.config.furnace.blasting_nuggets}x #forge:nuggets/${material}`,
+            `#forge:raw_materials/${material}`
+        );
+    }
+
+    function melting(inputTag, fluid, fluidAmount, heatRequirement) {
+        // Resolve optional parameters
+        fluidAmount =
+            typeof fluidAmount !== "undefined"
+                ? fluidAmount
+                : global.config.melting.amount;
+        heatRequirement =
+            typeof heatRequirement !== "undefined"
+                ? heatRequirement
+                : global.config.melting.heatRequirement;
+        // Get processing Time from FluidAmount
+        const processingTime = Math.max(
+            global.config.melting.minTime,
+            fluidAmount * global.config.melting.time
+        );
+        event.custom({
+            type: "createmetallurgy:melting",
+            // Might change later
+            ingredients: [{ tag: inputTag }],
+            processingTime: processingTime,
+            results: [
+                {
+                    fluid: fluid,
+                    amount: fluidAmount,
+                },
+            ],
+            heatRequirement: heatRequirement,
+        });
+    }
+});
