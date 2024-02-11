6c5cb9c76b47d22832df0850254d600a6567cbfd
Aiz <dev@aiz.moe>
2024-02-11 01:48:26 +0100

    feat(tags): rosin slimeball
    makes rosin work as a slimeball in some cases.

--- kubejs/server_scripts/item-tags.js
+++ kubejs/server_scripts/item-tags.js
@@ -1,6 +1,11 @@
 ServerEvents.tags("item", (event) => {
+    // To Unify rope
     event.add("forge:rope", "farmersdelight:rope");
     event.add("forge:rope", "quark:rope");
+
+    // rosin is a alternative to slimeballs
+    // not a replacement for the block though.
+    event.add("forge:slimeballs", "thermal:rosin");
 
     // Create crushed raw materials
     event
