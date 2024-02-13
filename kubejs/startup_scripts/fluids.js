9af11b665a4be9e62dd3fa5554fb865e709ad22a
Aiz <dev@aiz.moe>
2024-02-13 01:02:35 +0100

    feat(fluids): andesite mixture
    creates andesite mixture color is based on the darker spots on andesite alloy blocks.

--- kubejs/startup_scripts/fluids.js
+++ kubejs/startup_scripts/fluids.js
@@ -1,4 +1,15 @@
 StartupEvents.registry("fluid", (event) => {
+    //create fluids here
+
+    event.create(global.fluids.andesite_mixture).thinTexture(0x808582);
+    // Molten stuff
+    molten_metal(global.fluids.molten_tin, 0x7ea0b5);
+    molten_metal(global.fluids.molten_nickel, 0xbdac78);
+    molten_metal(global.fluids.molten_desh, 0xd68d4d);
+    molten_metal(global.fluids.molten_silver, 0xb4c0db);
+    molten_metal(global.fluids.molten_lead, 0x312d43);
+    molten_metal(global.fluids.molten_rose_gold, 0xf4af94);
+
     function molten_metal(fluid, color) {
         event
             .create(fluid)
@@ -6,12 +17,4 @@ StartupEvents.registry("fluid", (event) => {
             .flowingTexture("kubejs:block/fluid/molten/flowing")
             .color(color);
     }
-
-    //create fluids here
-    molten_metal(global.fluids.molten_tin, 0x7ea0b5);
-    molten_metal(global.fluids.molten_nickel, 0xbdac78);
-    molten_metal(global.fluids.molten_desh, 0xd68d4d);
-    molten_metal(global.fluids.molten_silver, 0xb4c0db);
-    molten_metal(global.fluids.molten_lead, 0x312d43);
-    molten_metal(global.fluids.molten_rose_gold, 0xf4af94);
 });
