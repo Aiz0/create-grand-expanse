b62ee7c6375080955e848a9ae8efc47e09105fee
Aiz <dev@aiz.moe>
2024-02-20 01:17:27 +0100

    feat(items): copper tools
    adds copper tools. no textures yet.

--- kubejs/startup_scripts/items.js
+++ kubejs/startup_scripts/items.js
@@ -1,4 +1,10 @@
 StartupEvents.registry("item", (event) => {
     // Create items here
     event.create(global.items.andesite_compound);
+
+    event.create(global.items.copper_pickaxe, "pickaxe").tier("copper");
+    event.create(global.items.copper_axe, "axe").tier("copper");
+    event.create(global.items.copper_shovel, "shovel").tier("copper");
+    event.create(global.items.copper_sword, "sword").tier("copper");
+    event.create(global.items.copper_hoe, "hoe").tier("copper");
 });
