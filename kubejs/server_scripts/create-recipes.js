44eb02a87f2f061349bb6a9563a80c06aaace67f
Aiz <dev@aiz.moe>
2024-02-29 00:47:58 +0100

    feat: bronze fluid.

--- kubejs/server_scripts/create-recipes.js
+++ kubejs/server_scripts/create-recipes.js
@@ -16,6 +16,21 @@ ServerEvents.recipes((event) => {
             },
         ],
     );
+    event.recipes.create
+        .mixing(
+            Fluid.of(global.fluids.molten_bronze, FluidAmounts.NUGGET * 1.5),
+            [
+                {
+                    fluid: global.fluids.molten_tin,
+                    amount: FluidAmounts.NUGGET,
+                },
+                {
+                    fluid: "createmetallurgy:molten_copper",
+                    amount: FluidAmounts.NUGGET * 2,
+                },
+            ],
+        )
+        .heated();
 
     // rubber from latex via mixing gives 1 extra rubber compared to handcrafting
     event.recipes.create.mixing(
