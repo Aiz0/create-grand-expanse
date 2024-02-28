130394c10e53069f5a2a4fd07fbefe36cb465d7e
Aiz <dev@aiz.moe>
2024-02-28 15:33:30 +0100

    refactor(kubejs): replacements.js -> furnace.js
    Replacements was actually not a good idea... moving furnace stuff in there.

--- kubejs/server_scripts/crafting.js
+++ kubejs/server_scripts/crafting.js
@@ -9,7 +9,59 @@ ServerEvents.recipes((event) => {
             C: "minecraft:clay_ball",
         },
     );
-    SmeltAndBlast("create:andesite_alloy", global.items.andesite_compound);
+
+    event
+        .shaped(
+            Item.of("sophisticatedbackpacks:backpack", 1),
+            ["ABA", "CDC", "EBE"],
+            {
+                A: "minecraft:leather",
+                B: "farmersdelight:canvas",
+                C: "#forge:rope",
+                D: "minecraft:barrel",
+                E: "minecraft:bundle",
+            },
+        )
+        .id("sophisticatedbackpacks:backpack");
+
+    // Mechanical Saw now uses thermal saw blade
+    event
+        .shaped(Item.of("create:mechanical_saw", 1), ["A  ", "BC "], {
+            A: "thermal:saw_blade",
+            B: "create:andesite_casing",
+            C: "create:shaft",
+        })
+        .id("create:crafting/kinetics/mechanical_saw");
+
+    // Mechanical Drill now uses thermal drill head
+    event
+        .shaped(Item.of("create:mechanical_drill", 1), ["A", "B", "C"], {
+            A: "thermal:drill_head",
+            B: "create:andesite_casing",
+            C: "create:shaft",
+        })
+        .id("create:crafting/kinetics/mechanical_drill");
+
+    // Empty Blaze Burner.
+    // Magma blocks instead of netherrack because it's easier to find in the overworld.
+    event
+        .shaped(
+            Item.of("create:empty_blaze_burner", 1),
+            ["AAA", "ABA", "CCC"],
+            {
+                A: "#forge:rods/iron",
+                B: "minecraft:magma_block",
+                C: "create:iron_sheet",
+            },
+        )
+        .id("create:crafting/kinetics/empty_blaze_burner");
+
+    event
+        .shaped(Item.of("create:fluid_tank", 1), ["ABA", "B B", "ABA"], {
+            A: "#forge:plates/copper",
+            B: "#forge:glass",
+        })
+        .id("create:crafting/kinetics/fluid_tank");
 
     // Thermal saw blade
     // crafting item
@@ -192,9 +244,4 @@ ServerEvents.recipes((event) => {
             B: "create:copper_casing",
         })
         .id("flopper:recipes/flopper");
-
-    function SmeltAndBlast(output, input) {
-        event.smelting(output, input);
-        event.blasting(output, input);
-    }
 });
