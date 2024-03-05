f456e78a6e89e5d98f6f7bc87ccd3f8823cfc871
Aiz <dev@aiz.moe>
2024-03-05 04:53:51 +0100

    style: use global.items instead of local var
    we created it so we are gonna use it.

--- kubejs/server_scripts/create-recipes.js
+++ kubejs/server_scripts/create-recipes.js
@@ -92,8 +92,8 @@ ServerEvents.recipes((event) => {
         const amount = item.includes("shaft")
             ? FluidAmounts.INGOT / 2
             : item.includes("large_cogwheel")
-              ? FluidAmounts.INGOT * 2
-              : FluidAmounts.INGOT;
+            ? FluidAmounts.INGOT * 2
+            : FluidAmounts.INGOT;
         event.recipes.create.filling(output, [
             Fluid.of("createmetallurgy:molten_copper", amount),
             item,
@@ -116,22 +116,24 @@ ServerEvents.recipes((event) => {
 
     event.remove({ id: "create:crafting/materials/electron_tube" });
 
-    let electron_tube_inter = "grand_expanse:incomplete_electron_tube";
     event.recipes.create
         .sequenced_assembly(["create:electron_tube"], "create:iron_sheet", [
-            event.recipes.createDeploying(electron_tube_inter, [
-                electron_tube_inter,
-                "#forge:glass",
-            ]),
-            event.recipes.createDeploying(electron_tube_inter, [
-                electron_tube_inter,
-                "create:polished_rose_quartz",
-            ]),
-            event.recipes.createFilling(electron_tube_inter, [
-                electron_tube_inter,
+            event.recipes.createDeploying(
+                global.items.incomplete_electron_tube,
+                [global.items.incomplete_electron_tube, "#forge:glass"],
+            ),
+            event.recipes.createDeploying(
+                global.items.incomplete_electron_tube,
+                [
+                    global.items.incomplete_electron_tube,
+                    "create:polished_rose_quartz",
+                ],
+            ),
+            event.recipes.createFilling(global.items.incomplete_electron_tube, [
+                global.items.incomplete_electron_tube,
                 Fluid.of(global.fluids.molten_rose_gold, global.INGOT),
             ]),
         ])
-        .transitionalItem(electron_tube_inter)
+        .transitionalItem(global.items.incomplete_electron_tube)
         .loops(1);
 });
