60d644b1156c53d39044ddfec0ad977441e8f92d
Aiz <dev@aiz.moe>
2024-03-11 02:34:40 +0100

    feat(worldgen): wolframite instead of silver on moon

--- kubejs/startup_scripts/world-gen.js
+++ kubejs/startup_scripts/world-gen.js
@@ -1,9 +1,12 @@
 WorldgenEvents.add((event) => {
     // Moon
     event.addOre((ore) => {
-        ore.id = global.packName + ":silver_ore_moon";
+        ore.id = global.packName + ":wolframite_ore_moon";
         //ore.biomes = "ad_astra:lunar_wastelands"; // should not be needed due to target
-        ore.addTarget("#ad_astra:moon_ore_replaceables", "thermal:silver_ore");
+        ore.addTarget(
+            "#ad_astra:moon_ore_replaceables",
+            "createmetallurgy:wolframite_ore",
+        );
         ore.count([10, 30]).squared().triangleHeight(-30, 85);
     });
     event.addOre((ore) => {
