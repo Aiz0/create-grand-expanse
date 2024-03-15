1566f5294ac66ab75f2d4c7c2559435f83aecae7
Aiz <dev@aiz.moe>
2024-03-15 10:24:50 +0100

    feat(ponder): electrolyzer ponder stuff
    still wip but it's better now

--- kubejs/client_scripts/ponder.js
+++ kubejs/client_scripts/ponder.js
@@ -75,11 +75,57 @@ Ponder.registry((event) => {
         .scene(
             "kinetic_electrolyzer",
             "How to make a kinectic electrolyzer",
+            "electrolyzer",
             (scene, util) => {
                 scene.showBasePlate();
-                scene
-                    .text(100, "WIP", [2, 2.5, 2.5])
-                    .colored(PonderPalette.RED);
+
+                // Animate in the 3x3 cube of copper casings and copper coil blocks
+                for (let z = 5; z >= 3; z--) {
+                    for (let y = 1; y <= 3; y++) {
+                        for (let x = 2; x <= 4; x++) {
+                            // Skip shaft for now
+                            if (x == 3 && y == 2) {
+                                continue;
+                            }
+                            scene.world.showSection([x, y, z], Facing.DOWN);
+                            scene.idle(2);
+                        }
+                    }
+                }
+                scene.idle(10);
+                // Fade in Tanks and electrolyzer
+                fadeIn(scene, [4, 1, 2, 4, 3, 2], Facing.DOWN, 5);
+                fadeIn(scene, [2, 1, 2, 2, 3, 2], Facing.DOWN, 10);
+                // Electrolyzer
+                fadeIn(scene, [3, 2, 2], Facing.DOWN, 20);
+
+                // Hide top part to show where shafts should go
+                const coil_top = [4, 3, 3, 2, 3, 5];
+                scene.world.hideSection(coil_top, Facing.UP);
+                scene.idle(20);
+                // Show Gears for electrolyzer part
+                fadeIn(scene, [3, 2, 7, 4, 1, 7], Facing.DOWN, 4);
+                // Animate in shafts
+                for (let z = 6; z >= 3; z--) {
+                    fadeIn(scene, [3, 2, z], Facing.DOWN, 4);
+                }
+                // Re show top part again
+                fadeIn(scene, coil_top, Facing.DOWN, 4);
+
+                // Show water pipes
+                // gears
+                fadeIn(scene, [5, 2, 7, 6, 1, 7], Facing.DOWN, 4);
+                // Animate in shafts
+                for (let z = 6; z >= 3; z--) {
+                    fadeIn(scene, [6, 1, z], Facing.DOWN, 4);
+                }
+                // final gear
+                scene.world.showSection([6, 2, 3], Facing.DOWN);
+
+                // Pipes
+                scene.world.showSection([7, 1, 2, 6, 5, 2], Facing.DOWN);
+                scene.world.showSection([5, 6, 2, 3, 4, 2], Facing.DOWN);
+                scene.world.showSection([3, 3, 2], Facing.DOWN);
             },
         );
 });
